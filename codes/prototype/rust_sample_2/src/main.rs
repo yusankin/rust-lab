ab7b1a5f7c07012e1ae6fe23da548396dc2adc64
yusankin <masuda.yu.business@gmail.com>
2025-04-03 21:08:50 +0900

    Rust training(詳解Rustプログラミング)update [p74~78]

--- codes/prototype/rust_sample_2/src/main.rs
+++ codes/prototype/rust_sample_2/src/main.rs
@@ -252,17 +252,88 @@
 //     }
 // }
 
-// リスト２−２０　インデックス変数を自動でインクリメントする
+// // リスト２−２０　インデックス変数を自動でインクリメントする
+// fn main() {
+//     let search_term = "picture";
+//     let quote = "\
+//     Every face, every shop, bedroom window, public-house, and
+//     dark square is a picture feverishly turned--in search of what?
+//     It is the same with books.
+//     What do we seek through millions of pages?";
+//     for (i, line) in quote.lines().enumerate() {
+//         if line.contains(search_term) {
+//             let line_num = i + 1; //行番号を求める。ステップごとに計算する必要はない。
+//             println!("{}:{}", line_num, line);
+//         }
+//     }
+// }
+
+// // リスト２−２１　配列定義と要素の繰り返し
+// fn main() {
+//     let one = [1, 2, 3];
+//     let two: [u8; 3] = [1, 2, 3];
+//     let blank1 = [0; 3];
+//     let blank2: [u8; 3] = [0; 3];
+
+//     let arrays = [one, two, blank1, blank2];
+
+//     for a in &arrays {
+//         print!("{:?}: ", a);
+//         for n in a.iter() {
+//             print!("\t{} + 10 = {}", n, n + 10);
+//         }
+
+//         let mut sum = 0;
+//         for i in 0..a.len() {
+//             sum += a[i];
+//         }
+//         println!("\t(Σ{:?} = {})", a, sum);
+//     }
+// }
 fn main() {
-    let search_term = "picture";
-    let quote = "\
-    Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
+    let ctx_lines = 2;
+    let needle = "oo";
+    let haystack = "\
+    Every face, every shop, 
+    bedroom window, public-house, and
+    dark square is a picture 
+    feverishly turned--in search of what?
     It is the same with books.
-    What do we seek through millions of pages?";
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1; //行番号を求める。ステップごとに計算する必要はない。
+    What do we seek 
+    through millions of pages?";
+    // tagsにマッチした各行の番号を入れる
+    let mut tags: Vec<usize> = vec![];
+    // ctxにコンテクスト行を格納したマッチごとのベクターを入れる
+    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+    // linesのイテレーションで、マッチがあった行番号を記録
+    for (i, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(i);
+            // Vec::with_capacity(n)で、n個の要素のための空間を予約
+            let v = Vec::with_capacity(2 * ctx_lines + 1);
+            ctx.push(v);
+        }
+    }
+
+    if tags.is_empty() {
+        return;
+    }
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(ctx_lines);
+            let upper_bound = tag + ctx_lines;
+
+            if (i >= lower_bound) && (i <= upper_bound) {
+                let line_as_string = String::from(line);
+                let local_ctx = (i, line_as_string);
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+    for local_ctx in ctx.iter() {
+        for &(i, ref line) in local_ctx.iter() {
+            let line_num = i + 1;
             println!("{}:{}", line_num, line);
         }
     }
